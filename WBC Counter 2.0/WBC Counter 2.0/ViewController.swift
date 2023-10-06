185c500cbad4c62b05ec9a8a14fda05f3c61c6db
Vladislav <vladisgnatenko@gmail.com>
2023-10-06 10:36:02 +0300

    first comit
    let`s go

---
+++ WBC Counter 2.0/WBC Counter 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBC Counter 2.0
+//
+//  Created by Vlad Ihnatenko on 06.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
